a3cf06b04bf2eefb604113a7216637e1c0c2fff0
Maxim Koshovyi <bocchitheprog@gmail.com>
2026-04-23 16:24:18 +0300

    Add description field for Recipe model

--- app/src/main/java/com/example/menuplanner/data/model/Recipe.kt
+++ app/src/main/java/com/example/menuplanner/data/model/Recipe.kt
@@ -7,6 +7,7 @@ import java.util.UUID
 data class Recipe (
     val id: UUID,
     val title: String,
+    val description: String,
     val calories: Int,
     val prepTimeMinutes: Int,
     val isVegetarian: Boolean,
