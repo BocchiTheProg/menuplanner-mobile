e16ac67ad6bcc1b68e4438ac43bcdfc0936b64b6
Maxim Koshovyi <bocchitheprog@gmail.com>
2026-05-06 23:59:15 +0300

    Create Data access object (DAO) to manipulate database

---
+++ app/src/main/java/com/example/menuplanner/data/MenuDao.kt
@@ -0,0 +1,30 @@
+package com.example.menuplanner.data
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Transaction
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface MenuDao {
+    // Recipes
+    @Query("SELECT * FROM recipes")
+    fun getAllRecipes(): Flow<List<RecipeEntity>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertRecipe(recipe: RecipeEntity)
+
+    @Delete
+    suspend fun deleteRecipe(recipe: RecipeEntity)
+
+    // Meal Plans
+    @Transaction
+    @Query("SELECT * FROM meal_plans")
+    fun getAllMealPlans(): Flow<List<MealPlanWithRecipes>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertMealPlan(mealPlan: MealPlanEntity)
+}
