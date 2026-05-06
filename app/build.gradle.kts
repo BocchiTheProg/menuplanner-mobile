2a9eaaed88aed04ae7ccd3dfdc4f82522c077cc9
Maxim Koshovyi <bocchitheprog@gmail.com>
2026-05-06 23:57:44 +0300

    Add plugins and dependency for room compiler

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
+    id("com.google.devtools.ksp")
 }
 
 android {
@@ -41,6 +42,7 @@ android {
 
 dependencies {
     implementation(libs.room.runtime)
+    ksp(libs.androidx.room.compiler)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
